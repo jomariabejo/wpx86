f623d6103168ae5566b823420cf33ab7f585acb5
jomari abejo <jomariabejo@gmail.com>
2023-05-21 23:59:16 +0800

    first commit

---
+++ helloworld.ps1
@@ -0,0 +1,22 @@
+﻿$hi = "Hello, World!"
+
+$myName = "Jomari Abejo"
+
+$greeting = $hi + " " + $myName
+
+Write-Host $greeting
+
+
+# Print Router Information
+Get-NetRoute -AddressFamily IPv4
+# Test Connectivity
+Test-Connection -ComputerName 192.168.254.254
+
+
+
+Get-CimInstance -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true |
+    Select-Object -ExpandProperty IPAddress
+
+
+Get-CimInstance -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true |
+    Get-Member -Name IPAddress
